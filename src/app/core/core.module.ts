1facb0261e66418e69c43c6d0dc0d87d582f5cb8
christophe <christophegueroult@gmail.com>
2021-06-14 16:29:46 +0200

    etape 13.2 imports modules and coremodule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UiModule } from '../ui/ui.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { IconsModule } from '../icons/icons.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [UiModule, TemplatesModule, IconsModule],
 })
-export class CoreModule { }
+export class CoreModule {}
