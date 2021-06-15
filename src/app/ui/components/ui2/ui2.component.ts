28c4ff54464d6f450a9095670295197e5936fc36
christophe <christophegueroult@gmail.com>
2021-06-15 15:33:29 +0200

    etape 12 ui2 agilite

---
+++ src/app/ui/components/ui2/ui2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui2',
+  templateUrl: './ui2.component.html',
+  styleUrls: ['./ui2.component.scss'],
+})
+export class Ui2Component implements OnInit {
+  public open = true;
+  constructor() {}
+  ngOnInit(): void {}
+  public toggle(): void {
+    this.open = !this.open;
+  }
+}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './components/ui/ui.component';
 import { CoreModule } from '../core/core.module';
+import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
-  declarations: [UiComponent],
+  declarations: [UiComponent, Ui2Component],
   imports: [CommonModule],
-  exports: [UiComponent],
+  exports: [UiComponent, Ui2Component],
 })
 export class UiModule {}
