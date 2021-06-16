1713e1c4391eed1b635da30e874ad23c0deeb561
christophe <christophegueroult@gmail.com>
2021-06-16 15:15:58 +0200

    etape 21 exemple Observable, Subject and BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 /**
  * this class is component who bootstrap application
  */
@@ -8,8 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  /**
-   * this property is an exemple for interpolation in html
-   */
-  public title = 'formation-angular-atos-juin';
+  private obs = new Observable((listXsubscribe) => {
+    listXsubscribe.next(Math.random());
+  });
+
+  private sub = new Subject();
+  private beh = new BehaviorSubject(0);
+
+  constructor() {
+    this.obs.subscribe((data) => {
+      console.log(data);
+    });
+    this.obs.subscribe((data) => {
+      console.log(data);
+    });
+    this.sub.next('test');
+    this.sub.next('test2');
+    this.sub.subscribe((data) => {
+      console.log(data);
+    });
+    this.sub.subscribe((data) => {
+      console.log(data);
+    });
+    this.sub.next(Math.random());
+    this.sub.subscribe((data) => {
+      console.log(data);
+    });
+
+    this.beh.next(Math.random());
+    this.beh.subscribe((data) => {
+      console.log(data);
+    });
+    this.beh.subscribe((data) => {
+      console.log(data);
+    });
+    this.beh.next(2);
+  }
 }
