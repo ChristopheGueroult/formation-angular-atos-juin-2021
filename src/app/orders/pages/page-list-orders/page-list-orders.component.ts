11b872814d407eaa7d07bf7a6011ec408c91d60b
christophe <christophegueroult@gmail.com>
2021-06-16 17:30:45 +0200

    etape 26 @Input() and data binding from parent to child component

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -7,9 +7,14 @@ import { OrdersService } from '../../services/orders.service';
   styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent implements OnInit {
+  public myTitle = 'List Orders';
   constructor(private ordersService: OrdersService) {
     this.ordersService.collection.subscribe();
   }
 
   ngOnInit(): void {}
+
+  public changeTitle(): void {
+    this.myTitle = 'My List Orders';
+  }
 }
