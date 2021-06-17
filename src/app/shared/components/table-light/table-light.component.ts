96ccaa44694dbbe4a8d908a0de567a5f36930332
christophe <christophegueroult@gmail.com>
2021-06-17 12:16:44 +0200

    etape 26 ng-content pour table light dynamique

--- src/app/shared/components/table-light/table-light.component.ts
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-table-light',
@@ -7,19 +6,8 @@ import { Order } from 'src/app/core/models/order';
   styleUrls: ['./table-light.component.scss'],
 })
 export class TableLightComponent implements OnInit {
-  @Input() public collection!: Order[];
   @Input() public headers!: string[];
-  constructor() {
-    console.log(this.headers);
-    console.log(this.collection);
-  }
+  constructor() {}
 
-  ngOnChanges(): void {
-    console.log(this.headers);
-    console.log(this.collection);
-  }
-  ngOnInit(): void {
-    console.log(this.headers);
-    console.log(this.collection);
-  }
+  ngOnInit(): void {}
 }
